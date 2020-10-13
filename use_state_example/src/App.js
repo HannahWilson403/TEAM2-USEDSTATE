e5dbededbf8cd7348fb4244b22530b0f61af1d2d
StewartNZ <elitestewartnz@gmail.com>
2020-10-14 10:07:38 +1300

    use state hook

--- use_state_example/src/App.js
+++ use_state_example/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [age, setAge] = useState(19);
+  const handleClick = () => setAge(age + 1)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div> 
+      I am {age} Years Old 
+      <div> 
+        <button onClick={handleClick}>Increase my age! </button>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
